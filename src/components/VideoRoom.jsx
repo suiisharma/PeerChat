713ac8672e611848a4aa63e9dd435571800e842d
sourav sharma <127027218+suiisharma@users.noreply.github.com>
2024-06-03 18:59:58 +0530

    Update VideoRoom.jsx

--- src/components/VideoRoom.jsx
+++ src/components/VideoRoom.jsx
@@ -315,12 +315,6 @@ const VideoRoom = () => {
     const reader = new FileReader();
     reader.onload = (e) => {
       const fileDataUrl = e.target.result;
-      const fileSizeInBytes = file.size;
-      const fileSizeInMB = fileSizeInBytes/(1024*1024);
-      if(fileSizeInMB>=261){
-        toast.error("File is too large!");
-        return ;
-      }
       // it contains the file as data url
       socket.emit("file",{roomJoinId, fileDataUrl, fileName: file.name });
       setMessages((prev) => {
